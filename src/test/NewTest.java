001fa812ccb6f0b3e816068911a5aa2c5148a5f9
admin <admin@C02RQBUDFVH3.local>
2017-10-16 15:23:25 +0800

    Demo

---
+++ src/test/NewTest.java
@@ -0,0 +1,31 @@
+package test;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.*;
+//java+selenium+testng+ant
+public class NewTest {
+  public  WebDriver driver;
+
+  @BeforeMethod
+  public void setup()
+  {
+	  driver=new  FirefoxDriver();  
+  }
+  @Test
+  public void login001() throws InterruptedException {
+	      driver.get("http://www.codoon.com/");
+	  	  Thread.sleep(5000);
+  }
+  @Test
+  public void login002() throws InterruptedException {
+	  driver.get("http://www.baidu.com/");
+	  Thread.sleep(5000);
+  }
+  @AfterMethod
+  public void teardown() throws InterruptedException {
+	  driver.quit(); 
+  }
+  
+}
